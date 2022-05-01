8881c81863ec58069ff0cf91119d38bfef00554e
iamsabbirsobhani <sabbirsobhani@hotmail.com>
2022-05-01 22:27:36 +0600

    indicator position fixed

--- src/components/TypingIndicator.jsx
+++ src/components/TypingIndicator.jsx
@@ -2,7 +2,7 @@ import "../styles/typing.scss";
 
 export default function TypingIndicator() {
   return (
-    <div class="ticontainer absolute -top-10 -left-1">
+    <div class="ticontainer absolute -top-3.5 -left-2">
       <div class="tiblock">
         <div class="tidot"></div>
         <div class="tidot"></div>
