dc4c186b8c081cfd42011cc35880f5bf418aa94c
iamsabbirsobhani <sabbirsobhani@hotmail.com>
2022-05-01 22:58:00 +0600

    Navbar fake name changed

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -4,7 +4,7 @@ export default function Navbar() {
     <div className=" text-white m-auto w-[350px] p-3 bg-gray-800/40">
       <div className=" flex items-center justify-center">
         <img className=" w-9 mr-3" src={avatar} alt="" />
-        <h1 className=" font-semibold text-xl">Rehan Wangsaff</h1>
+        <h1 className=" font-semibold text-xl">Albion Johnson</h1>
         <div className=" ml-3 border-none w-2  h-2 bg-green-500 rounded-full"></div>
       </div>
     </div>
--- src/components/TypingIndicator.jsx
+++ src/components/TypingIndicator.jsx
@@ -2,7 +2,7 @@ import "../styles/typing.scss";
 
 export default function TypingIndicator() {
   return (
-    <div class="ticontainer absolute -top-3.5 -left-2">
+    <div className="ticontainer absolute -top-3.5 -left-2">
       <div class="tiblock">
         <div class="tidot"></div>
         <div class="tidot"></div>
