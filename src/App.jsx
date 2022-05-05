fb51f48abcf264ea0f7fce94183a3b41a68818ac
iamsabbirsobhani <sabbirsobhani@hotmail.com>
2022-05-05 17:25:08 +0600

    Total user count emit event added

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,18 @@
 import Chat from "./views/Chat";
 import CallingTimer from "./components/CallingTimer";
 import { useSelector } from "react-redux";
+import { useEffect } from "react";
 
 function App(props) {
   const callTimer = useSelector((state) => state.global.callTimer);
   const pId = useSelector((state) => state.global.peerId);
+  useEffect(() => {
+    props.socket.emit("connect");
 
+    props.socket.on("total-user", (user) => {
+      console.log("Total user: ", user);
+    });
+  }, []);
   return (
     <div className="App">
       <header>
