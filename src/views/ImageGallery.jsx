371ae2b220cd6a875c46e2bfacaf52d0d06f3d4d
iamsabbirsobhani <sabbirsobhani@hotmail.com>
2022-05-08 20:10:13 +0600

    Refresh button added

--- src/views/ImageGallery.jsx
+++ src/views/ImageGallery.jsx
@@ -27,14 +27,12 @@ export default function ImageGallery() {
     } else {
       setimages(response.data);
       setimageError(null);
-      console.log(images);
       setisLogin(false);
     }
   }
 
   const handleSharedPictures = async (e) => {
     e.preventDefault();
-    console.log(e.target[0].value);
     setisLoading(true);
     await fetchImages(e.target[0].value);
     setisLoading(false);
@@ -42,6 +40,9 @@ export default function ImageGallery() {
 
   const openLogin = () => {
     setisLogin(!isLogin);
+  };
+  const resetGallery = () => {
+    setimages(null);
   };
 
   const handleClosePreview = (e) => {
@@ -105,6 +106,22 @@ export default function ImageGallery() {
               strokeLinecap="round"
               strokeLinejoin="round"
               d="M4 16l4.586-4.586a2 2 0 012.828 0L16 16m-2-2l1.586-1.586a2 2 0 012.828 0L20 14m-6-6h.01M6 20h12a2 2 0 002-2V6a2 2 0 00-2-2H6a2 2 0 00-2 2v12a2 2 0 002 2z"
+            />
+          </svg>
+        </button>
+        <button className=" ml-3" onClick={() => resetGallery()}>
+          <svg
+            xmlns="http://www.w3.org/2000/svg"
+            className="h-6 w-6"
+            fill="none"
+            viewBox="0 0 24 24"
+            stroke="currentColor"
+            strokeWidth={2}
+          >
+            <path
+              strokeLinecap="round"
+              strokeLinejoin="round"
+              d="M4 4v5h.582m15.356 2A8.001 8.001 0 004.582 9m0 0H9m11 11v-5h-.581m0 0a8.003 8.003 0 01-15.357-2m15.357 2H15"
             />
           </svg>
         </button>
