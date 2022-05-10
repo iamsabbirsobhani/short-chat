f848da5346b1cad7ee08380f96ae18d2d8b67d21
iamsabbirsobhani <sabbirsobhani@hotmail.com>
2022-05-10 15:44:47 +0600

    transcript styled

--- src/views/TranscriptChat.jsx
+++ src/views/TranscriptChat.jsx
@@ -12,6 +12,7 @@ export default function TranscriptChat() {
     );
     const finalRes = await response.data;
     setdata(finalRes);
+    console.log(finalRes);
   }
 
   //   execute only once after mounted
@@ -23,17 +24,26 @@ export default function TranscriptChat() {
   }, []);
   return (
     <>
-      <div className=" fixed top-14 bottom-0 break-words p-3 py-3 left-0 w-1/2 bg-gray-600 backdrop-blur-md overflow-y-scroll">
+      <div className=" fixed top-14 bottom-0 break-words p-3 py-3 left-0 w-1/2  backdrop-blur-md overflow-y-scroll">
         {data &&
           data.rows.map((chat) => (
-            <div className=" bg-gray-500 mb-3 p-2 rounded-sm">
-              <h1 className=" text-white font-semibold">{chat.name}</h1>
-              <div className=" break-words">
-                <p>{chat.msg}</p>
+            <div className="  mb-3 p-3 rounded-sm backdrop-blur-md border-[1px] border-gray-800">
+              <h1 className=" text-white antialiased tracking-wider">
+                {chat.name}
+              </h1>
+              <div className=" break-words bg-gray-800/20 p-2 rounded-sm">
+                <p className=" antialiased text-gray-100 font-semibold mt-3 mb-3">
+                  {chat.msg}
+                </p>
               </div>
               <div>
-                <p className=" text-xs text-gray-700">
+                <p className=" text-xs text-gray-600">
                   {format(new Date(chat.createdAt), "PPp")}
+                </p>
+                <p className=" text-xs text-gray-600">ID: {chat.id}</p>
+                <p className=" text-xs text-gray-600">IP: {chat.ip}</p>
+                <p className=" text-xs text-gray-600">
+                  location: {chat.location}
                 </p>
               </div>
             </div>
