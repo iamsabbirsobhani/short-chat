8ad531b9bc2b629feeb0bd8c8c6fa4137a4de62c
iamsabbirsobhani <sabbirsobhani@hotmail.com>
2023-04-16 07:03:20 +0600

    delete button loading state added

--- src/components/chat/ChatInfo.jsx
+++ src/components/chat/ChatInfo.jsx
@@ -47,9 +47,23 @@ export default function ChatInfo({ props }) {
         <h1>Sender: {chatInfo?.name}</h1>
         <p>Packet UID: {chatInfo?._id}</p>
         {chatInfo?.email ? <p>Email: {chatInfo?.email}</p> : null}
-        {chatInfo?.chat ? <p>Msg: {chatInfo?.chat}</p> : null}
+        {chatInfo?.chat ? (
+          <div>
+            <p>Msg:</p>
+            <div className=" bg-stone-900 p-2 rounded-sm break-words shadow-md">
+              {chatInfo?.chat}
+            </div>
+          </div>
+        ) : null}
         {chatInfo?.url ? <p>URL: {chatInfo?.url}</p> : null}
-        {token.id === 32 ? <p>Deleted Msg: {chatInfo?.deletedMsg}</p> : null}
+        {token.id === 32 && chatInfo?.deletedMsg ? (
+          <div>
+            <p>Deleted Msg:</p>
+            <div className=" bg-rose-600 p-2 rounded-sm shadow-md">
+              <h1 className="">{chatInfo?.deletedMsg}</h1>
+            </div>
+          </div>
+        ) : null}
         <p className="">
           Received: {format(new Date(chatInfo?.createdAt), 'PPPPpp')}
         </p>
@@ -57,26 +71,38 @@ export default function ChatInfo({ props }) {
         <div className="mt-5">
           {(token.id === chatInfo?.uId) &
           (chatInfo?.deletedMsg?.length <= 0) ? (
-            <button
-              onClick={handleDelete}
-              className="flex items-center text-red-500 bg-gray-50 font-bold p-2 rounded-sm shadow-md"
-            >
-              <h1>Delete!</h1>
-              <svg
-                xmlns="http://www.w3.org/2000/svg"
-                fill="none"
-                viewBox="0 0 24 24"
-                strokeWidth={1.5}
-                stroke="currentColor"
-                className=" w-4 h-4"
+            !delLoading ? (
+              <button
+                onClick={handleDelete}
+                className="flex items-center text-red-500 bg-gray-50 font-bold p-2 rounded-sm shadow-md"
               >
-                <path
-                  strokeLinecap="round"
-                  strokeLinejoin="round"
-                  d="M14.74 9l-.346 9m-4.788 0L9.26 9m9.968-3.21c.342.052.682.107 1.022.166m-1.022-.165L18.16 19.673a2.25 2.25 0 01-2.244 2.077H8.084a2.25 2.25 0 01-2.244-2.077L4.772 5.79m14.456 0a48.108 48.108 0 00-3.478-.397m-12 .562c.34-.059.68-.114 1.022-.165m0 0a48.11 48.11 0 013.478-.397m7.5 0v-.916c0-1.18-.91-2.164-2.09-2.201a51.964 51.964 0 00-3.32 0c-1.18.037-2.09 1.022-2.09 2.201v.916m7.5 0a48.667 48.667 0 00-7.5 0"
-                />
-              </svg>
-            </button>
+                <h1>Delete!</h1>
+                <svg
+                  xmlns="http://www.w3.org/2000/svg"
+                  fill="none"
+                  viewBox="0 0 24 24"
+                  strokeWidth={1.5}
+                  stroke="currentColor"
+                  className=" w-4 h-4"
+                >
+                  <path
+                    strokeLinecap="round"
+                    strokeLinejoin="round"
+                    d="M14.74 9l-.346 9m-4.788 0L9.26 9m9.968-3.21c.342.052.682.107 1.022.166m-1.022-.165L18.16 19.673a2.25 2.25 0 01-2.244 2.077H8.084a2.25 2.25 0 01-2.244-2.077L4.772 5.79m14.456 0a48.108 48.108 0 00-3.478-.397m-12 .562c.34-.059.68-.114 1.022-.165m0 0a48.11 48.11 0 013.478-.397m7.5 0v-.916c0-1.18-.91-2.164-2.09-2.201a51.964 51.964 0 00-3.32 0c-1.18.037-2.09 1.022-2.09 2.201v.916m7.5 0a48.667 48.667 0 00-7.5 0"
+                  />
+                </svg>
+              </button>
+            ) : (
+              <button
+                className="flex items-center text-red-500 bg-gray-50 font-bold p-2 rounded-sm shadow-md"
+                disabled
+              >
+                <h1>Deleting...</h1>
+                <div className="animate-spin">
+                  <ion-icon name="reload"></ion-icon>
+                </div>
+              </button>
+            )
           ) : null}
         </div>
       </div>
