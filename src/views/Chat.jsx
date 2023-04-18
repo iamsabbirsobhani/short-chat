61b42f2b4152687845c33c53f87f575372ef8c36
iamsabbirsobhani <sabbirsobhani@hotmail.com>
2023-04-18 22:39:18 +0600

    Single Chat Components
    in Chat components all functionalities added because of scroll to bottom is not working properly in seperated components

--- src/views/Chat.jsx
+++ src/views/Chat.jsx
@@ -68,8 +68,8 @@ export default function Chat(props) {
   const messagesEndRef = useRef(null);
 
   const scrollToBottom = () => {
-    // messagesEndRef.current?.scrollIntoView();
-    messagesEndRef.current?.scrollTo(0, messagesEndRef.current.scrollHeight);
+    //  scroll to bottom no smotth scroll without scrollIntoView
+    messagesEndRef.current.scrollIntoView();
   };
 
   const handleUpload = async (e) => {
@@ -153,9 +153,9 @@ export default function Chat(props) {
   };
 
   // task: try to fix scroll to bottom
-  useEffect(() => {
-    scrollToBottom();
-  });
+  // useEffect(() => {
+  //   scrollToBottom();
+  // });
 
   useEffect(() => {
     props.socket.on('is-there-only-users', (data) => {
@@ -324,6 +324,46 @@ export default function Chat(props) {
     dispatch(setChatInfo(m));
     // console.log(m);
   };
+
+  // handle blur self
+  const [isBlurSelf, setisBlurSelf] = useState({ isBlur: true, id: '' });
+  const [isLoadedSelf, setisLoadedSelf] = useState(false);
+  const handleImgOnLoadSelf = () => {
+    console.log('Image Loaded!');
+    setisLoadedSelf(true);
+  };
+
+  const handleBlurImageSelf = (id) => {
+    setisBlurSelf({ isBlur: !isBlurSelf.isBlur, id: id });
+    // console.log(id);
+  };
+
+  // after 30 seconds, the image will be blurred
+  useEffect(() => {
+    const timer = setTimeout(() => {
+      setisBlurSelf({ isBlur: true, id: '' });
+    }, 10000);
+    return () => clearTimeout(timer);
+  });
+  // handle blur Other
+  const [isBlurOther, setisBlurOther] = useState({ isBlur: true, id: '' });
+  const [isLoadedOther, setisLoadedOther] = useState(false);
+  const handleImgOnLoadOther = () => {
+    // console.log('Image Loaded!');
+    setisLoadedOther(true);
+  };
+
+  const handleBlurImageOther = (id) => {
+    setisBlurOther({ isBlur: !isBlurOther.isBlur, id: id });
+  };
+
+  // after 30 seconds, the image will be blurred
+  useEffect(() => {
+    const timer = setTimeout(() => {
+      setisBlurOther({ isBlur: true, id: '' });
+    }, 10000);
+    return () => clearTimeout(timer);
+  });
   return (
     <>
       <Navbar callSend={callSend} socket={props.socket} />
@@ -342,7 +382,7 @@ export default function Chat(props) {
         </div>
 
         {/* all Chats component */}
-        <ChatConroller
+        {/* <ChatConroller
           msg={msg}
           handleOlderMessage={handleOlderMessage}
           olderMsgLoading={olderMsgLoading}
@@ -350,8 +390,261 @@ export default function Chat(props) {
           messagesEndRef={messagesEndRef}
           handleSelfChatInfo={handleSelfChatInfo}
           handleOtherChatInfo={handleOtherChatInfo}
-        />
+        /> */}
+
+        {/* total chat parts */}
+        {msg.length ? (
+          <div className="grid grid-cols-2 gap-2">
+            {!olderMsgLoading ? (
+              <div
+                onClick={handleOlderMessage}
+                className="border-2 col-span-2 border-gray-50 rounded-full w-32 mt-5 mb-5 ml-auto mr-auto p-2 cursor-pointer"
+              >
+                <h1 className="text-gray-50 text-center text-xs">
+                  Older Messages
+                </h1>
+              </div>
+            ) : (
+              <div>
+                <div className=" z-50 fixed w-full h-full left-0 right-0 bottom-0 top-0  ml-auto mr-auto flex justify-center items-center bg-gray-400/50">
+                  <h1 className="text-gray-50 text-center text-lg font-bold">
+                    Loading...
+                  </h1>
+                </div>
+              </div>
+            )}
+            {msg.map((m, index) =>
+              token && m.uId == token.id ? (
+                m && m.deletedMsg ? (
+                  m.chat ? (
+                    <div
+                      onClick={() => handleSelfChatInfo(m)}
+                      ref={messagesEndRef}
+                      className=" cursor-pointer relative col-start-1 col-end-4    text-gray-300 border-gray-300  border-2 p-3 rounded-lg xl:max-w-full lg:max-w-full max-w-[300px]  break-words shadow-md ml-auto mr-1"
+                      key={index}
+                    >
+                      <h1 className=" mt-1 mb-1 first-letter:uppercase italic font-medium">
+                        {m.chat}
+                      </h1>
+                    </div>
+                  ) : (
+                    <div
+                      onClick={() => handleSelfChatInfo(m)}
+                      ref={messagesEndRef}
+                      className=" cursor-pointer relative col-start-1 col-end-4    text-gray-300 border-gray-300  border-2 p-3 rounded-lg xl:max-w-full lg:max-w-full max-w-[300px]  break-words shadow-md ml-auto mr-1"
+                      key={index}
+                    >
+                      <h1 className=" mt-1 mb-1 first-letter:uppercase italic font-medium">
+                        Image/Video/Audio has been unsent.
+                      </h1>
+                    </div>
+                  )
+                ) : m && m.url ? (
+                  <div
+                    ref={messagesEndRef}
+                    className=" flex flex-row-reverse items-center col-start-1 col-end-4"
+                    key={index}
+                  >
+                    <div className="cursor-pointer relative     text-white bg-emerald-700 p-3 rounded-lg xl:max-w-full lg:max-w-full md:max-w-[400px] max-w-[300px] break-words shadow-md ml-auto mr-1">
+                      {m.url &&
+                      m.url.includes('mp4') &&
+                      m.url.includes('video') ? (
+                        <video width="320" height="240" muted controls>
+                          <source src={m.url} type="video/mp4" />
+                          Your browser does not support the video tag.
+                        </video>
+                      ) : m.url && m.url.includes('audio') ? (
+                        <audio controls className=" w-44">
+                          <source src={m.url} type="audio/ogg" />
+                        </audio>
+                      ) : m.url && m.url.includes('images') ? (
+                        <div className=" rounded-md mt-1 mb-1 relative">
+                          {!isLoadedSelf ? (
+                            <div className=" w-72 h-60"></div>
+                          ) : null}
+                          <img
+                            className=""
+                            loading="lazy"
+                            src={m.url}
+                            alt=""
+                            style={{
+                              filter: isBlurSelf.isBlur
+                                ? 'blur(20px)'
+                                : isBlurSelf.id === m._id
+                                ? 'blur(0px)'
+                                : 'blur(20px)',
+                              transition: 'all 0.24s',
+                            }}
+                            onLoad={handleImgOnLoadSelf}
+                          />
+                          {isLoadedSelf ? (
+                            <div
+                              style={{
+                                visibility: isBlurSelf.isBlur
+                                  ? 'visible'
+                                  : isBlurSelf.id === m._id
+                                  ? 'hidden'
+                                  : 'visible',
+                              }}
+                              className="absolute top-0 bottom-0 left-0 right-0 flex items-center justify-center"
+                            >
+                              <button
+                                onClick={() => handleBlurImageSelf(m._id)}
+                                className=" border-2 rounded-full text-yellow-500 p-2 border-yellow-500 italic hover:text-yellow-600 hover:border-yellow-600 duration-300 shadow-md backdrop-blur-lg font-bold"
+                              >
+                                Tap to see clear
+                              </button>
+                            </div>
+                          ) : null}
+                        </div>
+                      ) : null}
+                    </div>
+                    <div
+                      onClick={() => handleSelfChatInfo(m)}
+                      className=" ml-4 cursor-pointer text-gray-50 mr-4"
+                    >
+                      <ion-icon name="ellipsis-horizontal"></ion-icon>
+                    </div>
+                  </div>
+                ) : m && m.chat ? (
+                  <div
+                    onClick={() => handleSelfChatInfo(m)}
+                    ref={messagesEndRef}
+                    className=" cursor-pointer relative col-start-1 col-end-4    text-white bg-emerald-700 p-3 rounded-lg xl:max-w-full lg:max-w-full md:max-w-[400px] max-w-[300px] break-words shadow-md ml-auto mr-1"
+                    key={index}
+                  >
+                    {m.chat &&
+                    m.chat.includes('mp4') &&
+                    m.chat.includes('video') ? (
+                      <video width="320" height="240" muted controls>
+                        <source src={m.chat} type="video/mp4" />
+                        Your browser does not support the video tag.
+                      </video>
+                    ) : (
+                      <h1 className=" mt-1 mb-1 ">{m.chat}</h1>
+                    )}
+                  </div>
+                ) : null
+              ) : m && m.deletedMsg ? (
+                m.chat ? (
+                  <div
+                    onClick={() => handleOtherChatInfo(m)}
+                    ref={messagesEndRef}
+                    className=" cursor-pointer relative col-start-1 col-end-4    text-gray-300 border-gray-300  border-2 p-3 rounded-lg xl:max-w-full lg:max-w-full max-w-[300px]  break-words shadow-md mr-auto ml-1 "
+                    key={index}
+                  >
+                    <h1 className=" mt-1 mb-1 first-letter:uppercase italic font-medium">
+                      {m.chat}
+                    </h1>
+                  </div>
+                ) : (
+                  <div
+                    onClick={() => handleOtherChatInfo(m)}
+                    ref={messagesEndRef}
+                    className=" cursor-pointer relative col-start-1 col-end-4    text-gray-300 border-gray-300  border-2 p-3 rounded-lg xl:max-w-full lg:max-w-full max-w-[300px]  break-words shadow-md mr-auto ml-1"
+                    key={index}
+                  >
+                    <h1 className=" mt-1 mb-1 first-letter:uppercase italic font-medium">
+                      Image/Video/Audio has been unsent.
+                    </h1>
+                  </div>
+                )
+              ) : m && m.url ? (
+                <div
+                  ref={messagesEndRef}
+                  className=" flex  items-center col-start-1 col-end-4"
+                  key={index}
+                >
+                  <div className=" cursor-pointer ml-1 mr-auto relative float-left text-white  bg-gray-800 p-3 rounded-lg xl:max-w-full lg:max-w-full md:max-w-[400px] max-w-[300px]  break-words shadow-md">
+                    {m.url &&
+                    m.url.includes('mp4') &&
+                    m.url.includes('video') ? (
+                      <video width="320" height="240" muted controls>
+                        <source src={m.url} type="video/mp4" />
+                        Your browser does not support the video tag.
+                      </video>
+                    ) : m.url && m.url.includes('audio') ? (
+                      <audio controls className=" w-44">
+                        <source src={m.url} type="audio/ogg" />
+                      </audio>
+                    ) : m.url && m.url.includes('images') ? (
+                      <div className="rounded-md mt-1 mb-1 relative">
+                        {!isLoadedOther ? (
+                          <div className=" w-72 h-60"></div>
+                        ) : null}
+                        <img
+                          className=""
+                          loading="lazy"
+                          src={m.url}
+                          alt=""
+                          style={{
+                            filter: isBlurOther.isBlur
+                              ? 'blur(20px)'
+                              : isBlurOther.id === m._id
+                              ? 'blur(0px)'
+                              : 'blur(20px)',
+                            transition: 'all 0.24s',
+                          }}
+                          onLoad={handleImgOnLoadOther}
+                        />
+                        {isLoadedOther ? (
+                          <div
+                            style={{
+                              visibility: isBlurOther.isBlur
+                                ? 'visible'
+                                : isBlurOther.id === m._id
+                                ? 'hidden'
+                                : 'visible',
+                            }}
+                            className="absolute top-0 bottom-0 left-0 right-0 flex items-center justify-center"
+                          >
+                            <button
+                              onClick={() => handleBlurImageOther(m._id)}
+                              className=" border-2 rounded-full text-yellow-500 p-2 border-yellow-500 italic hover:text-yellow-600 hover:border-yellow-600 duration-300 shadow-md backdrop-blur-lg font-bold"
+                            >
+                              Tap to see clear
+                            </button>
+                          </div>
+                        ) : null}
+                      </div>
+                    ) : null}
+                  </div>
+                  <div
+                    onClick={() => handleOtherChatInfo(m)}
+                    className=" ml-4 cursor-pointer text-gray-50 mr-4"
+                  >
+                    <ion-icon name="ellipsis-horizontal"></ion-icon>
+                  </div>
+                </div>
+              ) : m && m.chat ? (
+                <div
+                  onClick={() => handleOtherChatInfo(m)}
+                  className="col-start-1 col-end-4  cursor-pointer ml-1 mr-auto relative float-left text-white  bg-gray-800 p-3 rounded-lg xl:max-w-full lg:max-w-full md:max-w-[400px] max-w-[300px]  break-words shadow-md"
+                  ref={messagesEndRef}
+                  key={index}
+                >
+                  {m.chat &&
+                  m.chat.includes('mp4') &&
+                  m.chat.includes('video') ? (
+                    <video width="320" height="240" muted controls>
+                      <source src={m.chat} type="video/mp4" />
+                      Your browser does not support the video tag.
+                    </video>
+                  ) : (
+                    <h1 className=" mt-1 mb-1 ">{m.chat}</h1>
+                  )}
+                </div>
+              ) : null,
+            )}
+          </div>
+        ) : (
+          <div className=" text-white text-2xl flex h-full w-full  justify-center items-center">
+            Loading...
+          </div>
+        )}
       </div>
+      {/* end of total chat parts */}
+
       {/* Chat typing form and indicator component */}
       <ChatTypingForm
         isTypings={isTypings}
@@ -364,7 +657,6 @@ export default function Chat(props) {
         handleChat={handleChat}
         id={id}
         handleUpload={handleUpload}
-        scrollToBottom={scrollToBottom}
       />
       <Routes>
         <Route
